73dff879b8404b13894a74aed64640623d7e0cba
Mickaël Anderson <123479860+RaMiAnderson@users.noreply.github.com>
2023-07-11 23:28:42 +0300

    Novaina page multiple page ilay single page , raison Mr TOM, et creation route de ces page, User sisa

--- controller/adminController.js
+++ controller/adminController.js
@@ -8,6 +8,56 @@ const getHomepage = (req,res) => {
     else res.redirect('auth/login');
 }
 
+const getAbsence = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if(stateConnection) res.render('admin/pages/adminAbsence')
+    else res.redirect('auth/login');
+}
+
+const getAnnonce = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminAnnonce")
+    else res.redirect("auth/login");
+}
+
+const getApayer = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminApayer")
+    else res.redirect("auth/login");
+}
+
+const getCalendar = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminCalendar")
+    else res.redirect("auth/login");
+}
+
+const getEdt = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminEdt")
+    else res.redirect("auth/login");
+}
+
+const getGerer = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminGerer")
+    else res.redirect("auth/login");
+}
+
+const getNote = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminNote")
+    else res.redirect("auth/login");
+}
+
+const getNotif = (req,res) => {
+    const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
+    if (stateConnection) res.render("admin/pages/adminNotif")
+    else res.redirect("auth/login");
+}
+
+
+
 const addFiliere = (req , res) => {
     const stateConnection = authentificationService.verifyIfAlreadyConnected(req);
     if (stateConnection) res.render('admin/post/filiere')
@@ -23,6 +73,14 @@ const addMatiere = (req,res) => {
 
 module.exports = {
     getHomepage,
+    getAbsence,
+    getAnnonce,
+    getApayer,
+    getCalendar,
+    getEdt,
+    getGerer,
+    getNote,
+    getNotif,
     addFiliere,
     addMatiere
 }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -4,9 +4,32 @@ const adminController = require("../controller/adminController");
 const authController = require("../controller/authController")
 
 
-// Get Page
+
+//------------- Get Page ------------
 
 router.get("/",adminController.getHomepage);
+
+router.get("/absence" , adminController.getAbsence);
+
+router.get("/annonce" , adminController.getAnnonce);
+
+router.get("/apayer" , adminController.getApayer);
+
+router.get("/calendrier" , adminController.getCalendar);
+
+router.get("/edt" , adminController.getEdt);
+
+router.get("/gerer" , adminController.getGerer);
+
+router.get("/note" , adminController.getNote);
+
+router.get("/notification" , adminController.getNotif);
+
+
+
+
+
+
 
 // page des formulaire 
 
@@ -16,7 +39,9 @@ router.get('/add-matiere' , adminController.addMatiere );
 
 
 
-//Post 
+
+
+//---------------------- Post --------------- 
 
 
 
